786d912c8895d1df1a27325e111b12ba5ecf3206
Donald Johnson <donaldrjohnson66@gmail.com>
2022-11-24 12:05:39 -0600

    finished

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Numerics;
 
 namespace ArraysAndLists
 {
@@ -9,11 +12,13 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
-
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            List<int> evens = new List<int>();
+            List<int> odds = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +26,64 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
 
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+                    
+
+
+
+
+
+
+
+
+
+
+            }
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            for (int i = 0; i < evens.Count; i++)
+            {
+                Console.WriteLine($"{evens[i]} is a even number.");
+                
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < odds.Count; i++)
+            {
+                Console.WriteLine($"{odds[i]} is a odd number.");
+                
+            }
+            
+
+            
+               
+                
+
+                
+            
+                
+                    
+                
+            
+            
+
+            
+
+            
         }
     }
 }
